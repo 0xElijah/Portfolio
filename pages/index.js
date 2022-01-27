bb73ec418df732626b17c7f1710097e1759c8000
0xElijah <elijah@diffusion.me>
2022-01-26 21:38:09 -0800

    mroe styling

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
 
       {/* Me */}
       <div className="max-w-screen-md mx-auto px-10 sm:px-5 mt-[-40px] sm:mt-[-100px]">
-        <div className="flex justify-between items-end pt-7 pb-7 sm:pb-20">
+        <div className="flex justify-between items-end pt-7 pb-10">
           <ProfileImg />
           <div className="pl-7 sm:pb-3 sm:mb-10 z-50">
             <h1 class="text-3xl sm:text-5xl font-bold pb-1 sm:pb-3">
@@ -48,31 +48,26 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Intro */}
-        <div className="pb-10">
-          {/* <h2 className="text-lg sm:text-2xl text-center font-bold pb-7">
-            Hello World
-          </h2>
-          Eli here, a full stack and Rust developer looking to join a team of
-          builders in the Solana ecosystem! Hit me up if you are hiring: */}
-          <div className="flex flex-col justify-around p-5 sm:flex-row">
+        {/* Contact */}
+        <div className="pb-20">
+          <div className="flex flex-col items-center justify-around sm:p-5 sm:flex-row">
             <a
               href={`https://` + TWITTER}
               target="_blank"
-              className="flex text-lg font-bold m-2"
+              className="flex font-bold m-2"
             >
               <IconContext.Provider
-                value={{ className: 'mr-3', color: '0C72B7', size: 30 }}
+                value={{ className: 'mr-3', color: '0C72B7', size: 25 }}
               >
                 <FaTwitter /> {TWITTER}
               </IconContext.Provider>
             </a>
             <div
               onClick={() => copyUsername()}
-              className="flex font-bold text-lg cursor-pointer m-2"
+              className="flex font-bold cursor-pointer m-2"
             >
               <IconContext.Provider
-                value={{ className: 'mr-3', color: '5865F2', size: 30 }}
+                value={{ className: 'mr-3', color: '5865F2', size: 25 }}
               >
                 <FaDiscord />
               </IconContext.Provider>
@@ -83,9 +78,6 @@ export default function Home() {
 
         {/* Portfolio */}
         <div className="pb-20">
-          {/* <h2 className="text-lg sm:text-2xl text-center font-bold pb-7">
-            Web3 Portfolio
-          </h2> */}
           <div>
             <h3 className="font-bold sm:text-lg">NFT Viewer</h3>
             <p>Simple gallery of NFTs in a given Solana wallet</p>
@@ -93,9 +85,7 @@ export default function Home() {
         </div>
 
         {/* NFTs */}
-        <div className="pb-10">
-          <h2 className="text-lg sm:text-2xl font-bold pb-2">My NFTs</h2>
-        </div>
+        <div className="pb-10"></div>
       </div>
     </div>
   );
